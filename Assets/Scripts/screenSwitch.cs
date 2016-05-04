4917b16d3e1021ef1282bdc9e50a58b3bc4fd664
PohSayKeong <poh.saykeong@dhs.sg>
2016-05-04 20:16:25 +0800

    Initial
    Screen resolution is set at 960x600.

---
+++ Assets/Scripts/screenSwitch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class screenSwitch : MonoBehaviour {
+
+    public void startGame() {
+
+        SceneManager.LoadScene("Main");
+    }
+}
