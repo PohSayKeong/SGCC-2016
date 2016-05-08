b6ec088200ac38838f7cf69142d076b8bc8bb8bd
PohSayKeong <poh.saykeong@dhs.sg>
2016-05-08 14:21:19 +0800

    Added toothbrush and start button

---
+++ Assets/Scripts/rotateScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateScript : MonoBehaviour
+{
+    public int speed;
+    public float friction;
+    public float lerpSpeed;
+    private float xDeg;
+    private float yDeg;
+    private Quaternion fromRotation;
+    private Quaternion toRotation;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            xDeg -= Input.GetAxis("Mouse X") * speed * friction;
+            yDeg += Input.GetAxis("Mouse Y") * speed * friction;
+            fromRotation = transform.rotation;
+            toRotation = Quaternion.Euler(yDeg, xDeg, 0);
+            transform.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * lerpSpeed);
+        }
+
+
+    }
+}
